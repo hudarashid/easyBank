630f9592bc2568a61927a416ea7107b6a40136b7
hudarashid <hudarashid87@gmail.com>
2021-06-17 18:06:30 +0800

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,30 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function() {
+    console.log('click hamburger');
+//_header.scss contain class '.open'
+//add or change to header & overlay classes when click hamburger
+    
+    if(header.classList.contains('open')) { //close hamburger menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+        
+    }
+    else { //open hamburger menu
+        body.classList.add('noscroll');
+        header.classList.add('open'); 
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+        
+    }
+});
